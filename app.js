586f7047867dc7c1585b397835f4b78873d36bfa
DeviozTI <deviozti@gmail.com>
2024-12-01 16:32:14 -0500

    Añadir manejador para la ruta raíz

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const { connectToMySQL } = require('./db/mySQL');
-const { connectToSQLServer } = require('./db/sqlServer'); // Solo para local
-const syncRoutes = require('./routes/sync');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Verificar si la conexión es local o en Render
-if (process.env.NODE_ENV !== 'production') {
-  connectToSQLServer(); // Solo conecta a SQL Server si no estás en producción
-}
-
-// Siempre conecta a MySQL
+// Conectar a MySQL
 connectToMySQL();
 
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a mi API!'); // Mensaje para la ruta raíz
+});
+
+// Rutas específicas (por ejemplo, /api/sync)
+const syncRoutes = require('./routes/sync');
 app.use('/api', syncRoutes);
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
